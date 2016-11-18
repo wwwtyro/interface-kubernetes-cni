2d27fac5c7e8c78db92e0740da68dbae713ef36e
Rye Terrell <ryeterrell@ryeterrell.net>
2016-11-18 14:50:35 -0600

    initial

---
+++ provides.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+from charms.reactive import RelationBase
+from charms.reactive import hook
+from charms.reactive import scopes
+
+
+class CNIPluginProvider(RelationBase):
+    scope = scopes.UNIT
+
+    @hook('{provides:cni-plugin}-relation-{joined,changed}')
+    def joined_or_changed(self):
+        ''' Set the connected state from the provides side of the relation. '''
+        conv = self.conversation()
+        conv.set_state('{relation_name}.connected')
+        if conv.get_remote('available') is True:
+            conv.set_state('{relation_name}.available')
+
+    @hook('{provides:cni-plugin}-relation-{departed}')
+    def broken_or_departed(self):
+        '''Remove connected state from the provides side of the relation. '''
+        conv = self.conversation()
+        conv.remove_state('{relation_name}.connected')
+        conv.remove_state('{relation_name}.available')
+
+    def set_config(self, is_master, kubeconfig_path):
+        ''' Relays a dict of kubernetes configuration information. '''
+        conv = self.conversation()
+        conv.set_remote(data = {
+            'is_master': is_master,
+            'kubeconfig_path': kubeconfig_path
+        })
---
+++ requires.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+from charms.reactive import RelationBase
+from charms.reactive import hook
+from charms.reactive import scopes
+
+
+class CNIPluginClient(RelationBase):
+    scope = scopes.UNIT
+
+    @hook('{requires:cni-plugin}-relation-{joined,changed}')
+    def changed(self):
+        ''' Indicate the relation is connected, and if the relation data is
+        set it is also available. '''
+        conv = self.conversation()
+        conv.set_state('{relation_name}.connected')
+        config = self.get_config()
+        if config['is_master'] is True:
+            self.set_state('{relation_name}.is-master')
+        elif config['is_master'] is False:
+            self.set_state('{relation_name}.is-worker')
+
+    @hook('{requires:cni-plugin}-relation-{departed}')
+    def broken(self):
+        ''' Indicate the relation is no longer available and not connected. '''
+        self.remove_state('{relation_name}.connected')
+        # TODO: Remove more states?
+
+    def get_config(self):
+        ''' Get the kubernetes configuration information. '''
+        conv = self.conversation()
+        return {
+            'is_master': conv.get_remote('is_master'),
+            'kubeconfig_path': conv.get_remote('kubeconfig_path')
+        }
+
+    def set_available(self):
+        conv = self.conversation()
+        conv.set_remote(data = {
+            'available': True
+        })
