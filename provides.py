c3ba378ce048acfe0228b4595bb552b2598d275c
George Kraft <cynerva@gmail.com>
2016-11-21 13:32:29 -0600

    use global scope

--- provides.py
+++ provides.py
@@ -6,27 +6,24 @@ from charms.reactive import scopes
 
 
 class CNIPluginProvider(RelationBase):
-    scope = scopes.UNIT
+    scope = scopes.GLOBAL
 
     @hook('{provides:kubernetes-cni}-relation-{joined,changed}')
     def joined_or_changed(self):
         ''' Set the connected state from the provides side of the relation. '''
-        conv = self.conversation()
-        conv.set_state('{relation_name}.connected')
-        if conv.get_remote('available') is True:
-            conv.set_state('{relation_name}.available')
+        self.set_state('{relation_name}.connected')
+        if self.get_remote('available') is True:
+            self.set_state('{relation_name}.available')
 
     @hook('{provides:kubernetes-cni}-relation-{departed}')
     def broken_or_departed(self):
         '''Remove connected state from the provides side of the relation. '''
-        conv = self.conversation()
-        conv.remove_state('{relation_name}.connected')
-        conv.remove_state('{relation_name}.available')
+        self.remove_state('{relation_name}.connected')
+        self.remove_state('{relation_name}.available')
 
     def set_config(self, is_master, kubeconfig_path):
         ''' Relays a dict of kubernetes configuration information. '''
-        conv = self.conversation()
-        conv.set_remote(data = {
+        self.set_remote(data = {
             'is_master': is_master,
             'kubeconfig_path': kubeconfig_path
         })
--- requires.py
+++ requires.py
@@ -6,14 +6,13 @@ from charms.reactive import scopes
 
 
 class CNIPluginClient(RelationBase):
-    scope = scopes.UNIT
+    scope = scopes.GLOBAL
 
     @hook('{requires:kubernetes-cni}-relation-{joined,changed}')
     def changed(self):
         ''' Indicate the relation is connected, and if the relation data is
         set it is also available. '''
-        conv = self.conversation()
-        conv.set_state('{relation_name}.connected')
+        self.set_state('{relation_name}.connected')
         config = self.get_config()
         if config['is_master'] is True:
             self.set_state('{relation_name}.is-master')
@@ -28,14 +27,10 @@ class CNIPluginClient(RelationBase):
 
     def get_config(self):
         ''' Get the kubernetes configuration information. '''
-        conv = self.conversation()
         return {
-            'is_master': conv.get_remote('is_master'),
-            'kubeconfig_path': conv.get_remote('kubeconfig_path')
+            'is_master': self.get_remote('is_master'),
+            'kubeconfig_path': self.get_remote('kubeconfig_path')
         }
 
     def set_available(self):
-        conv = self.conversation()
-        conv.set_remote(data = {
-            'available': True
-        })
+        self.set_remote('available', True)
