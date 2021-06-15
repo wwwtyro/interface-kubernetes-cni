294927949c63ea969b6eb3a67f79057145946945
Cory Johns <johnsca@gmail.com>
2021-06-15 16:54:45 -0400

    Switch to GitHub Actions, use charms.unit_test, add Black, fix existing lint and tests

--- tests/test_requires.py
+++ tests/test_requires.py
@@ -4,20 +4,17 @@ import requires
 
 
 def test_get_config():
-    client = requires.CNIPluginClient()
-    config = {
-        'is_master': False
-    }
+    client = requires.CNIPluginClient("test")
+    config = {"is_master": False}
     client.all_joined_units.received_raw = config
     assert client.get_config() == config
 
 
 def test_set_config():
-    client = requires.CNIPluginClient()
+    client = requires.CNIPluginClient("test")
     client.relations = [MagicMock(), MagicMock()]
-    client.set_config('192.168.0.0/24', '10-test.conflist')
+    client.set_config("192.168.0.0/24", "10-test.conflist")
     for relation in client.relations:
-        relation.to_publish_raw.update.assert_called_once_with({
-            'cidr': '192.168.0.0/24',
-            'cni-conf-file': '10-test.conflist'
-        })
+        relation.to_publish_raw.update.assert_called_once_with(
+            {"cidr": "192.168.0.0/24", "cni-conf-file": "10-test.conflist"}
+        )
